1071fbbec0ec0ca9e304852317125e97c12a2df9
RobertLRollins <robertlrollins@protonmail.com>
2024-02-08 04:51:53 -0500

    Add placeToken, GameController

--- ttt.js
+++ ttt.js
@@ -22,19 +22,88 @@ function Gameboard() {
         console.log(boardWithCellValues);
     }
 
+    function placeToken(row, column, player) {
+
+        if (row < 0 || row >= board.length || column < 0 || column >= board[0].length) {
+            console.log("Invalid cell. Please choose a valid row and column.");
+            return;
+        }
+
+
+        const cell = board[row][column];
+
+
+        if (cell.getValue() !== "") {
+            console.log("This cell is already occupied. Please choose another cell.");
+            return;
+        }
+
+
+        cell.addToken(player);
+    }
+
     return {
-        printBoard: printBoard
+        printBoard: printBoard,
+        placeToken: placeToken
     };
 }
 
 function Cell() {
-    let value = 0;
+    let value = "";
+
+    function addToken(player) {
+        value = player;
+    }
+
+    function getValue() {
+        return value;
+    }
 
     return {
-        getValue: function () {
-            return value;
-        },
+        addToken: addToken,
+        getValue: getValue
     };
 }
 
-Gameboard().printBoard(); 
+function GameController(playerOneName, playerTwoName) {
+    playerOneName = playerOneName || "Player One";
+    playerTwoName = playerTwoName || "Player Two";
+
+    var board = Gameboard();
+    var players = [
+        { name: playerOneName, token: "X" },
+        { name: playerTwoName, token: "O" }
+    ];
+
+    var activePlayer = players[0];
+
+    function switchPlayerTurn() {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    }
+
+    function getActivePlayer() {
+        return activePlayer;
+    }
+
+    function printNewRound() {
+        board.printBoard();
+        console.log(getActivePlayer().name + "'s turn.");
+    }
+
+    function playRound(row, column) {
+        console.log(`Placing ${getActivePlayer().name}'s token at row ${row}, column ${column}...`);
+        board.placeToken(row, column, getActivePlayer().token);
+
+        switchPlayerTurn();
+        printNewRound();
+    }
+
+    printNewRound();
+
+    return {
+        playRound: playRound,
+        getActivePlayer: getActivePlayer
+    };
+}
+
+const game = GameController();
